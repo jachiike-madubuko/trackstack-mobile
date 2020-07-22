581eb5ef25d2b67ee50f42ff6548fffdae1e9a46
Jachiike Madubuko <jachiike.madubuko@gmail.com>
2020-07-21 21:53:33 -0500

    Initial commit

---
+++ fireconfig.js
@@ -0,0 +1,10 @@
+export const config = {
+  apiKey: "AIzaSyBsebQMTYquy2nxGZMlyYc4aHeU-UVy-bU",
+  authDomain: "dsbtk-banqa.firebaseapp.com",
+  databaseURL: "https://dsbtk-banqa.firebaseio.com",
+  projectId: "dsbtk-banqa",
+  storageBucket: "dsbtk-banqa.appspot.com",
+  messagingSenderId: "166632443995",
+  appId: "1:166632443995:web:478a73ae6589fea2962f7e",
+  measurementId: "G-2XC90WWS24"
+};
